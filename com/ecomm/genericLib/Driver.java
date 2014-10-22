1df297ef3a91003b3c7d57f09e44b7eec9c5ad5d
Anand Kumar Meher <anandmeher@users.noreply.github.com>
2014-10-22 19:03:24 +0530

    Create Driver.java

---
+++ com/ecomm/genericLib/Driver.java
@@ -0,0 +1,11 @@
+package com.ecomm.genericLib;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Driver {
+	
+	// Invoke FireFox Driver.
+	public static WebDriver driver = new FirefoxDriver();
+
+}

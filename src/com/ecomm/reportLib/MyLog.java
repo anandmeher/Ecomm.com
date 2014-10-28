0e66803c36ce0e74d2db68752fad59dc67d6b3dd
Anand Kumar Meher <anandmeher@users.noreply.github.com>
2014-10-28 19:47:51 +0530

    Update MyLog.java

--- src/com/ecomm/reportLib/MyLog.java
+++ src/com/ecomm/reportLib/MyLog.java
@@ -1,17 +1,16 @@
-package com.ecomm.reportLib;
+package com.ecomm.reprotLib;
 
 import org.apache.log4j.Logger;
 import org.apache.log4j.xml.DOMConfigurator;
 
 public class MyLog {
+		
+	static Logger log;
 	
-	public static void LogInfo(String infoMsg){
-		DOMConfigurator.configure("src/com/ecomm/reportLib/log4J.xml");
-		Logger log = Logger.getLogger(MyLog.class.getName());		
-		log.info(infoMsg);
+	public static Logger Log(){
+		DOMConfigurator.configure("src\\com\\ecomm\\reprotLib\\log4j.xml");
+		log = Logger.getLogger(MyLog.class.getName());		
+		return log;
 	}
-	
-	
-	
 
 }

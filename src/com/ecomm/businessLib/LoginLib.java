d49179f7c319e8994cc8cd934f4fcfe7cf5386c7
Anand Kumar Meher <anandmeher@users.noreply.github.com>
2014-10-28 19:47:17 +0530

    Update LoginLib.java

--- src/com/ecomm/businessLib/LoginLib.java
+++ src/com/ecomm/businessLib/LoginLib.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.support.PageFactory;
 
 import com.ecomm.genericLib.Driver;
 import com.ecomm.pageFactory.LoginPageLib;
-import com.ecomm.reportLib.MyLog;
+import com.ecomm.reprotLib.MyLog;
 
 public class LoginLib {
 	
@@ -12,17 +12,13 @@ public class LoginLib {
 		
 		LoginPageLib loginPageObj = PageFactory.initElements(Driver.driver, LoginPageLib.class);
 		
-		MyLog.LogInfo("Open Browser and Navigate to Login Page");
-		Driver.driver.get(loginPageObj.getURL());
-		Driver.driver.manage().window().maximize();
-		
-		MyLog.LogInfo("Clear Email Address Text field and enter email address");
+		MyLog.Log().info("Clear Email Address Text field and enter email address");
 		loginPageObj.getEmailAddressEdt().clear();
 		loginPageObj.getEmailAddressEdt().sendKeys("demo@avactis.com");
 		loginPageObj.getPasswordEdt().clear();
 		loginPageObj.getPasswordEdt().sendKeys("demo");
 		loginPageObj.getSignInBtn().click();
-		MyLog.LogInfo("Logged in to application");
+		MyLog.Log().info("Logged in to application");
 	}
 
 }

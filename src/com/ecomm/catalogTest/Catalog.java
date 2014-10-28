a7a5ea352a78c2278a8591f462277253c23c9b75
Anand Kumar Meher <anandmeher@users.noreply.github.com>
2014-10-28 19:46:22 +0530

    Update Catalog.java
    Implemented AfterTest and BeforeTest , handled exception , message in CMD,TestNg and log file

--- src/com/ecomm/catalogTest/Catalog.java
+++ src/com/ecomm/catalogTest/Catalog.java
@@ -1,31 +1,52 @@
 package com.ecomm.catalogTest;
 
+import org.openqa.selenium.support.PageFactory;
 import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import com.ecomm.businessLib.LoginLib;
 import com.ecomm.genericLib.Driver;
-import com.ecomm.reportLib.MyLog;
+import com.ecomm.pageFactory.LoginPageLib;
+import com.ecomm.reprotLib.MyLog;
 
 public class Catalog {
+	
+	LoginPageLib loginPageObj = PageFactory.initElements(Driver.driver, LoginPageLib.class);
+	
+	@BeforeTest
+	public void beforeTest(){
+		MyLog.Log().info("Open Browser and Navigate to Login Page");
+		Driver.driver.get(loginPageObj.getURL());
+		Driver.driver.manage().window().maximize();	
+	}
+	
+	@AfterTest
+	public void afterTest(){
+		Driver.driver.quit();
+		MyLog.Log().info("Browser is closed");
+	}
 	
   @Test
   public void TC001() {
 	 try {
-		  MyLog.LogInfo("LoginToApp is called");	  
+		  MyLog.Log().info("Executing TC001");	  
 		  LoginLib.LoginToApp("demo@avactis.com", "demo");
 		  String expTitle = "Home - Avactis ShoppingCart";
 		  String actualTitle = Driver.driver.getTitle();
 		  Assert.assertEquals(actualTitle, expTitle, "Title Mis Match");		
 		  
-	} catch (AssertionError e) {
-		
+	} catch (AssertionError ae) {
+		System.out.println("Inside Catch");
+		MyLog.Log().error(ae);
+		Assert.fail(ae.toString());
 	}   
   }
   
 @Test
   public void TC002(){
-	  Driver.driver.close();
-	  MyLog.LogInfo("Browser is closed");
+	  MyLog.Log().info("Executing TC002");
+	  
   }
 }

50bcd848f8ba0e1cf383a36bd00705e34f490d27
Anand Kumar Meher <anandmeher@users.noreply.github.com>
2014-10-30 22:50:09 +0530

    Update Catalog.java

--- src/com/ecomm/catalogTest/Catalog.java
+++ src/com/ecomm/catalogTest/Catalog.java
@@ -6,10 +6,11 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-import com.ecomm.businessLib.LoginLib;
+import com.ecomm.businessLib.LoginLogoutLib;
 import com.ecomm.genericLib.Driver;
 import com.ecomm.pageFactory.LoginPageLib;
-import com.ecomm.reprotLib.MyLog;
+import com.ecomm.reportLib.MyLog;
+import com.ecomm.reportLib.TestStatus;
 
 public class Catalog {
 	
@@ -17,36 +18,39 @@ public class Catalog {
 	
 	@BeforeTest
 	public void beforeTest(){
-		MyLog.Log().info("Open Browser and Navigate to Login Page");
+		MyLog.log().info("Open Browser and Navigate to Login Page");
 		Driver.driver.get(loginPageObj.getURL());
 		Driver.driver.manage().window().maximize();	
 	}
 	
 	@AfterTest
-	public void afterTest(){
+	public void AfterTest(){
+		MyLog.log().info("Quit Browser");
 		Driver.driver.quit();
-		MyLog.Log().info("Browser is closed");
 	}
 	
+	
   @Test
-  public void TC001() {
+  public void TC001() throws Exception {
 	 try {
-		  MyLog.Log().info("Executing TC001");	  
-		  LoginLib.LoginToApp("demo@avactis.com", "demo");
+		  MyLog.log().info("Executing TC001");	  
+		  LoginLogoutLib.LoginToApp("demo@avactis.com", "demo");
 		  String expTitle = "Home - Avactis ShoppingCart";
 		  String actualTitle = Driver.driver.getTitle();
-		  Assert.assertEquals(actualTitle, expTitle, "Title Mis Match");		
+		  Assert.assertEquals(actualTitle, expTitle, "Title Mis-Match");		
 		  
-	} catch (AssertionError ae) {
-		System.out.println("Inside Catch");
-		MyLog.Log().error(ae);
-		Assert.fail(ae.toString());
+	} catch (AssertionError e) {
+		
+		TestStatus.fail(e);
+		
 	}   
   }
   
-@Test
+  @Test
   public void TC002(){
-	  MyLog.Log().info("Executing TC002");
-	  
+	  MyLog.log().info("Executing TC002");
   }
+  
+  
+  
 }

faf27b32520811ee20702e85085d1b92cfb3885f
Anand Kumar Meher <anandmeher@users.noreply.github.com>
2014-11-12 12:35:20 +0530

    Update HomePageLib.java

--- src/com/ecomm/pageFactory/HomePageLib.java
+++ src/com/ecomm/pageFactory/HomePageLib.java
@@ -7,5 +7,18 @@ public class HomePageLib {
 	
 	@FindBy(linkText="Catalog")
 	private WebElement catalogLink; 
+	
+	@FindBy(linkText="Manage Products")
+	private WebElement manageProductsLink;
+
+	public WebElement getCatalogLink() {
+		return catalogLink;
+	}
+
+	public WebElement getManageProductsLink() {
+		return manageProductsLink;
+	}
+	
+	
 
 }

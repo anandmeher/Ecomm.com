e233d9335d455789fcfd413a33b4f125b7fb1ea3
Anand Kumar Meher <anandmeher@users.noreply.github.com>
2014-11-12 12:32:15 +0530

    Update LoginLogoutLib.java

--- src/com/ecomm/businessLib/LoginLogoutLib.java
+++ src/com/ecomm/businessLib/LoginLogoutLib.java
@@ -1,44 +1,38 @@
 package com.ecomm.businessLib;
 
 import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
 
 import com.ecomm.genericLib.Driver;
 import com.ecomm.pageFactory.HomePageLib;
 import com.ecomm.pageFactory.LoginPageLib;
 import com.ecomm.reportLib.MyLog;
 
-public class LoginLogoutLib {
-	
-	static LoginPageLib loginPageObj = PageFactory.initElements(Driver.driver, LoginPageLib.class);
-	static HomePageLib homePageObj = PageFactory.initElements(Driver.driver, HomePageLib.class);
+public class LoginLib {
 	
 	public static void LoginToApp(String emailAddress, String password){
 		
-		//LoginPageLib loginPageObj = PageFactory.initElements(Driver.driver, LoginPageLib.class);
 		try {
-			MyLog.log().info("Open Browser and Navigate to Login Page");
-			Driver.driver.get(loginPageObj.getURL());
-			Driver.driver.manage().window().maximize();
+			LoginPageLib loginPageObj = PageFactory.initElements(Driver.driver, LoginPageLib.class);
 			
-			MyLog.log().info("Clear Email Address Text field and enter email address");
+			MyLog.Log().info("Clear Email Address Text field and enter email address");
 			loginPageObj.getEmailAddressEdt().clear();
 			loginPageObj.getEmailAddressEdt().sendKeys("demo@avactis.com");
 			loginPageObj.getPasswordEdt().clear();
 			loginPageObj.getPasswordEdt().sendKeys("demo");
 			loginPageObj.getSignInBtn().click();
-			MyLog.log().info("Logged in to application");
+			MyLog.Log().info("Logged in to application");
 			
 		} catch (Exception e) {
-			MyLog.log().error(e.getMessage());
-			throw(e);
+			MyLog.Log().error(e.toString());
+			Assert.fail(e.toString());
 		}
-		
 		
 	}
 	
 	public static void LogoutApp(){
-	
-		homePageObj.getAdminInfoBtn().click();
+		
+		HomePageLib.getAdminInfoBtn().click();
 	}
 
 }
